ce568bfef0bfb9f67d69c09375d635bc8e65c4a9
Anmenig <35890296+anmenig@users.noreply.github.com>
2024-10-06 05:39:03 +0300

    Update lang.js

--- lang.js
+++ lang.js
@@ -1,26 +1,14 @@
 const langArr = {
-    "unit" :  {
-        "en": "build a multi language site ",
-        "uk": "створюємо багатомовний сайт",
+    "js-head" :  {
+        "en": "<meta charset="UTF-8">
+    <meta name=viewport content="width=device-width, initial-scale=1.0>
+    <title>ItGid.info EN</title>
+    <link rel=shortcut icon href=/favicon.ico type=image/x-icon>",
+        "uk": "<meta charset="UTF-8">
+    <meta name=viewport content="width=device-width, initial-scale=1.0>
+    <title>ItGid.info UK-UA</title>
+    <link rel=shortcut icon href=/favicon.ico type=image/x-icon>",
     }, 
-    "chip": {
-        "en": "graphics processing unit",
-        "uk": "графічний чіп",
-    }, 
-    "memory": {
-        "en": "memory size",
-        "uk": "oбсяг пам'яті",
-    }, 
-    "memory-type": {
-        "en": "memory type",
-        "uk": "nип пам'яті",
-    }, 
-    "cool": {
-        "en": "сooling system type",
-        "uk": "тип системи охолодження",
-    }, 
-    "more": {
-        "en": "more details",
-        "uk": "детально про товар",
-    }, 
+    
+    
 }
