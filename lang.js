ed1da8c66940cc78d9e49877fceb74f889c33e9d
Anmenig <35890296+anmenig@users.noreply.github.com>
2024-10-06 05:43:21 +0300

    Update lang.js

--- lang.js
+++ lang.js
@@ -1,10 +1,10 @@
 const langArr = {
     "js-head" :  {
-        "en": "<meta charset="UTF-8">
+        "en": "<meta charset=UTF-8>
     <meta name=viewport content="width=device-width, initial-scale=1.0>
     <title>ItGid.info EN</title>
     <link rel=shortcut icon href=/favicon.ico type=image/x-icon>",
-        "uk": "<meta charset="UTF-8">
+        "uk": "<meta charset=UTF-8>
     <meta name=viewport content="width=device-width, initial-scale=1.0>
     <title>ItGid.info UK-UA</title>
     <link rel=shortcut icon href=/favicon.ico type=image/x-icon>",
